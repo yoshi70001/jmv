989f36a6ba77d103f2bca128556ce16fe1b8bebc
JorgeEspinoza976 <jorge.espinoza@2win.cl>
2025-09-22 17:19:05 -0500

    refactor(config): optimiza la dependencia de lucide-react

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  // Ya no excluimos lucide-react, permitiendo que Vite lo optimice.
+  // optimizeDeps: {
+  //   exclude: ['lucide-react'],
+  // },
 });
